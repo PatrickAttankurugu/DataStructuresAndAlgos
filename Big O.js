9b2181b159de7f6c21d33233c0cece3ba15e030d
Patrick Attankurugu <patricka.azuma@gmail.com>
2022-08-31 12:21:05 +0000

    first commit

---
+++ Big O.js
@@ -0,0 +1,9 @@
+const nemo = ['John', 'Justice', 'nemo'];
+function findNemo(array) {
+  for (i = 0; i < nemo.lenght; i++) {
+    if (i === 'nemo') {
+      console.log('We have found nemo');
+    }
+  }
+}
+findNemo(nemo);
